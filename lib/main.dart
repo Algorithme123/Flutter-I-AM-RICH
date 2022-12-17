24750f9643bb5bfae894bdda9b1d277d0577292b
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2022-12-17 10:39:11 +0000

    Im rich

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,26 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("I am Rich"),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/eee.jpg'),
-            fit: BoxFit.cover,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am Rich"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/eee.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
